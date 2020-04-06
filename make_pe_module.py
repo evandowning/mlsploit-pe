de355cb99754d27d18a8cf15090c4dd8a617d1ad
Evan Downing <2077950+evandowning@users.noreply.github.com>
2020-04-06 04:50:52 +0000

    create yaml file generator script

---
+++ make_pe_module.py
@@ -0,0 +1,326 @@
+from mlsploit import Module
+
+# From https://github.com/mlsploit/mlsploit.github.io/blob/master/static/img/modules/pe.png
+IMG_URL = 'https://raw.githubusercontent.com/mlsploit/mlsploit.github.io/master/static/img/modules/pe.png'
+
+module = Module.build(
+    display_name='PE Module',
+    tagline='This is the PE module',
+    doctxt="""Module to train and attack PE models.""",
+    icon_url=IMG_URL)
+
+###########################################
+# Create model ensemble training function #
+###########################################
+function = module.build_function(
+    name='Ensemble - Train',
+    doctxt="""Trains an ensemble of models on PE features.""",
+    creates_new_files=True,
+    modifies_input_files=False,
+    expected_filetype='txt',
+    optional_filetypes=[])
+
+# API sequence
+function.add_option(
+    name='sequence',
+    type='bool',
+    default=False,
+    doctxt="""Sequence - Enable""",
+    required=True)
+function.add_option(
+    name='sequence_window',
+    type='int',
+    default=32,
+    doctxt="""Sequence - Window Size""",
+    required=True)
+function.add_option(
+    name='sequence_type',
+    type='enum',
+    enum_values=['multi_classification','binary_classification'],
+    default='binary_classification',
+    doctxt="""Sequence - Classification Type""",
+    required=True)
+function.add_option(
+    name='sequence_lstm',
+    type='bool',
+    default=False,
+    doctxt="""Sequence - LSTM - Enable""",
+    required=True)
+function.add_option(
+    name='sequence_rnn',
+    type='bool',
+    default=False,
+    doctxt="""Sequence - RNN - Enable""",
+    required=True)
+function.add_option(
+    name='sequence_cnn',
+    type='bool',
+    default=False,
+    doctxt="""Sequence - CNN - Enable""",
+    required=True)
+
+# API existence
+function.add_option(
+    name='existence',
+    type='bool',
+    default=False,
+    doctxt="""Existence - Enable""",
+    required=True)
+function.add_option(
+    name='existence_rf',
+    type='bool',
+    default=False,
+    doctxt="""Existence - Random Forest - Enable""",
+    required=True)
+function.add_option(
+    name='existence_rf_trees',
+    type='int',
+    default=10,
+    doctxt="""Existence - Random Forest - Number of Trees""",
+    required=True)
+function.add_option(
+    name='existence_nb',
+    type='bool',
+    default=False,
+    doctxt="""Existence - Naive Bayes - Enable""",
+    required=True)
+function.add_option(
+    name='existence_knn',
+    type='bool',
+    default=False,
+    doctxt="""Existence - k-Nearest Neighbors - Enable""",
+    required=True)
+function.add_option(
+    name='existence_knn_k',
+    type='int',
+    default=5,
+    doctxt="""Existence - k-Nearest Neighbors - k-value""",
+    required=True)
+function.add_option(
+    name='existence_sgd',
+    type='bool',
+    default=False,
+    doctxt="""Existence - Stochastic Gradient Descent - Enable""",
+    required=True)
+function.add_option(
+    name='existence_mlp',
+    type='bool',
+    default=False,
+    doctxt="""Existence - Multi-Layer Perceptron - Enable""",
+    required=True)
+
+# API frequency
+function.add_option(
+    name='frequency',
+    type='bool',
+    default=False,
+    doctxt="""Frequency - Enable""",
+    required=True)
+function.add_option(
+    name='frequency_rf',
+    type='bool',
+    default=False,
+    doctxt="""Frequency - Random Forest - Enable""",
+    required=True)
+function.add_option(
+    name='frequency_rf_trees',
+    type='int',
+    default=10,
+    doctxt="""Frequency - Random Forest - Number of Trees""",
+    required=True)
+function.add_option(
+    name='frequency_nb',
+    type='bool',
+    default=False,
+    doctxt="""Frequency - Naive Bayes - Enable""",
+    required=True)
+function.add_option(
+    name='frequency_knn',
+    type='bool',
+    default=False,
+    doctxt="""Frequency - k-Nearest Neighbors - Enable""",
+    required=True)
+function.add_option(
+    name='frequency_knn_k',
+    type='int',
+    default=5,
+    doctxt="""Frequency - k-Nearest Neighbors - k-value""",
+    required=True)
+function.add_option(
+    name='frequency_sgd',
+    type='bool',
+    default=False,
+    doctxt="""Frequency - Stochastic Gradient Descent - Enable""",
+    required=True)
+function.add_option(
+    name='frequency_mlp',
+    type='bool',
+    default=False,
+    doctxt="""Frequency - Multi-Layer Perceptron - Enable""",
+    required=True)
+
+# API arguments
+function.add_option(
+    name='arguments',
+    type='bool',
+    default=False,
+    doctxt="""Arguments - Enable""",
+    required=True)
+function.add_option(
+    name='arguments_rf',
+    type='bool',
+    default=False,
+    doctxt="""Arguments - Random Forest - Enable""",
+    required=True)
+function.add_option(
+    name='arguments_rf_trees',
+    type='int',
+    default=10,
+    doctxt="""Arguments - Random Forest - Number of Trees""",
+    required=True)
+function.add_option(
+    name='arguments_nb',
+    type='bool',
+    default=False,
+    doctxt="""Arguments - Naive Bayes - Enable""",
+    required=True)
+function.add_option(
+    name='arguments_knn',
+    type='bool',
+    default=False,
+    doctxt="""Arguments - k-Nearest Neighbors - Enable""",
+    required=True)
+function.add_option(
+    name='arguments_knn_k',
+    type='int',
+    default=5,
+    doctxt="""Arguments - k-Nearest Neighbors - k-value""",
+    required=True)
+function.add_option(
+    name='arguments_sgd',
+    type='bool',
+    default=False,
+    doctxt="""Arguments - Stochastic Gradient Descent - Enable""",
+    required=True)
+function.add_option(
+    name='arguments_mlp',
+    type='bool',
+    default=False,
+    doctxt="""Arguments - Multi-Layer Perceptron - Enable""",
+    required=True)
+
+function.add_output_tag(name='tag1', type='str')
+
+#############################################
+# Create model ensemble evaluating function #
+#############################################
+function = module.build_function(
+    name='Ensemble - Evaluate',
+    doctxt="""Evaluates an ensemble of trained models.""",
+    creates_new_files=True,
+    modifies_input_files=False,
+    expected_filetype='txt',
+    optional_filetypes=['zip'])
+function.add_option(
+    name='sequence',
+    type='bool',
+    doctxt="""Sequence - Enable""",
+    required=True)
+function.add_option(
+    name='sequence_window',
+    type='int',
+    default=32,
+    doctxt="""Sequence - Window Size""",
+    required=True)
+function.add_option(
+    name='sequence_type',
+    type='enum',
+    enum_values=['multi_classification','binary_classification'],
+    default='binary_classification',
+    doctxt="""Sequence - Classification Type""",
+    required=True)
+function.add_option(
+    name='existence',
+    type='bool',
+    doctxt="""Existence - Enable""",
+    required=True)
+function.add_option(
+    name='frequency',
+    type='bool',
+    doctxt="""Frequency - Enable""",
+    required=True)
+function.add_option(
+    name='arguments',
+    type='bool',
+    doctxt="""Arguments - Enable""",
+    required=True)
+
+function.add_output_tag(name='tag1', type='str')
+
+##################################
+# Create mimicry attack function #
+##################################
+function = module.build_function(
+    name='Mimicry Attack',
+    doctxt="""Performs mimicry attack.""",
+    creates_new_files=True,
+    modifies_input_files=False,
+    expected_filetype='txt',
+    optional_filetypes=['zip'])
+function.add_option(
+    name='sequence',
+    type='bool',
+    doctxt="""Sequence - Enable""",
+    required=True)
+function.add_option(
+    name='sequence_window',
+    type='int',
+    default=32,
+    doctxt="""Sequence - Window Size""",
+    required=True)
+function.add_option(
+    name='sequence_type',
+    type='enum',
+    enum_values=['multi_classification','binary_classification'],
+    default='binary_classification',
+    doctxt="""Sequence - Classification Type""",
+    required=True)
+function.add_option(
+    name='generations',
+    type='int',
+    default=1,
+    doctxt="""Number of attack samples to generate""",
+    required=True)
+function.add_output_tag(name='tag1', type='str')
+
+##################################
+# Create PE transformer function #
+##################################
+function = module.build_function(
+    name='PE Transformer',
+    doctxt="""Statically modifies PE binary to exhibit attack.""",
+    creates_new_files=True,
+    modifies_input_files=False,
+    expected_filetype='zip',
+    optional_filetypes=[])
+function.add_option(
+    name='hash',
+    type='str',
+    doctxt="""SHA-256 hash value of binary to transform.""",
+    required=True)
+function.add_output_tag(name='tag1', type='str')
+
+########################################
+# Create trampoline detection function #
+########################################
+function = module.build_function(
+    name='Detect Trampoline',
+    doctxt="""Trains model to detect trampoline insertion.""",
+    creates_new_files=True,
+    modifies_input_files=False,
+    expected_filetype='txt',
+    optional_filetypes=[])
+function.add_output_tag(name='tag1', type='str')
+
+# Save yaml file
+module.save()
