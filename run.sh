1ed583a8d665533a4799d6fd683c9715d0fbf40e
Evan Downing <2077950+evandowning@users.noreply.github.com>
2020-04-28 04:05:33 +0000

    update modeling functions

--- run.sh
+++ run.sh
@@ -132,15 +132,12 @@ if [ "$NAME" = "Ensemble-Train" ]; then
         echo $END >> $LOG
         echo $END >> $LOG_ERR
 
-        #TODO
         # Train models
         if [ $SEQUENCE_LSTM = true ]; then
             echo "Training LSTM model" >> $LOG
             echo "Training LSTM model" >> $LOG_ERR
             echo "Start Timestamp: `date +%s`" >> $LOG
-            #python3 lstm.py "$EXTRACT/api.txt" "api-sequence-features/" "$OUTPUT/model/api-sequence/lstm/" True False $SEQUENCE_TYPE "$OUTPUT/model/api-sequence/lstm/convert_classes.txt" >> $LOG 2>> $LOG_ERR
-            python3 lstm.py "$EXTRACT/api.txt" "api-sequence-features/" "$OUTPUT/model/api-sequence/lstm/" True False $SEQUENCE_TYPE "$OUTPUT/model/api-sequence/lstm/convert_classes.txt"
-            echo "BLAH"
+            python3 lstm.py "$EXTRACT/api.txt" "api-sequence-features/" "$OUTPUT/model/api-sequence/lstm/" True False $SEQUENCE_TYPE "$OUTPUT/model/api-sequence/lstm/convert_classes.txt" >> $LOG 2>> $LOG_ERR
             echo "End Timestamp: `date +%s`" >> $LOG
             echo $END >> $LOG
             echo $END >> $LOG_ERR
@@ -150,9 +147,7 @@ if [ "$NAME" = "Ensemble-Train" ]; then
             echo "Training RNN model" >> $LOG
             echo "Training RNN model" >> $LOG_ERR
             echo "Start Timestamp: `date +%s`" >> $LOG
-            #python3 rnn.py "$EXTRACT/api.txt" "api-sequence-features/" "$OUTPUT/model/api-sequence/rnn/" True False $SEQUENCE_TYPE "$OUTPUT/model/api-sequence/rnn/convert_classes.txt" >> $LOG 2>> $LOG_ERR
-            python3 rnn.py "$EXTRACT/api.txt" "api-sequence-features/" "$OUTPUT/model/api-sequence/rnn/" True False $SEQUENCE_TYPE "$OUTPUT/model/api-sequence/rnn/convert_classes.txt"
-            echo "BLAH"
+            python3 rnn.py "$EXTRACT/api.txt" "api-sequence-features/" "$OUTPUT/model/api-sequence/rnn/" True False $SEQUENCE_TYPE "$OUTPUT/model/api-sequence/rnn/convert_classes.txt" >> $LOG 2>> $LOG_ERR
             echo "End Timestamp: `date +%s`" >> $LOG
             echo $END >> $LOG
             echo $END >> $LOG_ERR
@@ -162,9 +157,7 @@ if [ "$NAME" = "Ensemble-Train" ]; then
             echo "Training CNN model" >> $LOG
             echo "Training CNN model" >> $LOG_ERR
             echo "Start Timestamp: `date +%s`" >> $LOG
-            #python3 cnn.py "$EXTRACT/api.txt" "api-sequence-features/" "$OUTPUT/model/api-sequence/cnn/" True False $SEQUENCE_TYPE "$OUTPUT/model/api-sequence/cnn/convert_classes.txt" >> $LOG 2>> $LOG_ERR
-            python3 cnn.py "$EXTRACT/api.txt" "api-sequence-features/" "$OUTPUT/model/api-sequence/cnn/" True False $SEQUENCE_TYPE "$OUTPUT/model/api-sequence/cnn/convert_classes.txt"
-            echo "BLAH"
+            python3 cnn.py "$EXTRACT/api.txt" "api-sequence-features/" "$OUTPUT/model/api-sequence/cnn/" True False $SEQUENCE_TYPE "$OUTPUT/model/api-sequence/cnn/convert_classes.txt" >> $LOG 2>> $LOG_ERR
             echo "End Timestamp: `date +%s`" >> $LOG
             echo $END >> $LOG
             echo $END >> $LOG_ERR
